83a46338f45e29eaed2ef358be9941a5da849563
Eric Ridgeway <ericridgeway@gmail.com>
2023-10-28 23:40:30 -0400

    Kill scaff

--- lib/ao_c_shared.ex
+++ lib/ao_c_shared.ex
@@ -1,18 +1,2 @@
 defmodule AoCShared do
-  @moduledoc """
-  Documentation for `AoCShared`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> AoCShared.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
