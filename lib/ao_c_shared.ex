9eb47bdc0d20fcfbf45478c8ca319c50a4e4ba50
Eric Ridgeway <ericridgeway@gmail.com>
2023-10-28 23:41:16 -0400

    Copy shared module from latest AoC

--- lib/ao_c_shared.ex
+++ lib/ao_c_shared.ex
@@ -1,2 +1,25 @@
 defmodule AoCShared do
+  def clean(input) do
+    input
+    |> String.trim
+    |> String.replace("\r\n", "\n")
+  end
+
+  def clean_no_trim(input) do
+    input
+    |> String.replace("\r\n", "\n")
+  end
+
+  def split_rows(input) do
+    input
+    |> clean
+    |> String.split("\n")
+  end
+
+  def loop(n_times, starting_input, reducer) do
+    reducer_ignore_first_param = fn _, acc -> reducer.(acc) end
+    Enum.reduce(1..n_times, starting_input, reducer_ignore_first_param)
+  end
+
+  def a(day_number), do: File.read!("day#{day_number}.txt")
 end
