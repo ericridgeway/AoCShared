2ef51f9181224604600c84bb4f569f687d346300
Eric Ridgeway <ericridgeway@gmail.com>
2023-11-16 14:34:17 -0500

    Defp the function lists I might not need to be public

--- lib/ao_c_shared/coord.ex
+++ lib/ao_c_shared/coord.ex
@@ -11,16 +11,17 @@ defmodule AoCShared.Coord do
   def left_down(t), do: t |> left |> down
   def left_up(t), do: t |> left |> up
 
-  def cardinal_funcs, do: [&up/1, &down/1, &left/1, &right/1]
-  def diagonal_funcs, do: [&right_up/1, &right_down/1, &left_down/1, &left_up/1]
-  def all_adjacent_funcs, do: cardinal_funcs() ++ diagonal_funcs()
-
   def all_adjacent(t) do
     all_adjacent_funcs()
     |> Enum.reduce(MapSet.new, fn slide_direction, coord_set ->
       MapSet.put(coord_set, slide_direction.(t))
     end)
   end
+
+
+  defp cardinal_funcs, do: [&up/1, &down/1, &left/1, &right/1]
+  defp diagonal_funcs, do: [&right_up/1, &right_down/1, &left_down/1, &left_up/1]
+  defp all_adjacent_funcs, do: cardinal_funcs() ++ diagonal_funcs()
 
   # def neighbors(board, target) do
   #   board_coords =
