e00eeaf012342dbeea987baea6b2b43a96a27e6a
Eric Ridgeway <ericridgeway@gmail.com>
2023-11-17 07:09:07 -0500

    Copy over ascii code and quick small test

--- lib/ao_c_shared/ascii.ex
+++ lib/ao_c_shared/ascii.ex
@@ -1,3 +1,35 @@
 defmodule AoCShared.Ascii do
 
+  # def to_map(ascii) do
+  #   list_form =
+  #     ascii
+  #     |> AoCShared.clean
+  #     |> String.split("\n")
+  #     |> Enum.map(&String.graphemes/1)
+  #     |> Enum.map(fn row -> Enum.map(row, &String.to_integer/1) end)
+  #     |> Enum.reverse
+
+  #   for {row, row_num} <- Enum.with_index(list_form, 1),
+  #       {col, col_num} <- Enum.with_index(row, 1)
+  #   do
+  #     {{col_num, row_num}, col}
+  #   end
+  #   |> Map.new
+  # end
+
+  def to_map(ascii) do
+    list_form =
+      ascii
+      |> AoCShared.clean
+      |> String.split("\n")
+      |> Enum.map(&String.graphemes/1)
+      |> Enum.reverse
+
+    for {row, row_num} <- Enum.with_index(list_form, 1),
+        {col, col_num} <- Enum.with_index(row, 1)
+    do
+      {{col_num, row_num}, col}
+    end
+    |> Map.new
+  end
 end
--- test/ascii_test.exs
+++ test/ascii_test.exs
@@ -2,4 +2,16 @@ defmodule AoCSharedTest.Ascii do
   use ExUnit.Case
 
   alias AoCShared.Ascii
+
+  test "Ascii.to_map/1" do
+    board = """
+    123
+    ...
+    ...
+    """
+    |> Ascii.to_map
+
+    assert Map.get(board, {1,1}) == "."
+    assert Map.get(board, {3,3}) == "3"
+  end
 end
