a01ce85ea0e061553f598f52018dbfaa2a1e0439
Eric Ridgeway <ericridgeway@gmail.com>
2023-11-22 10:54:58 -0500

    Great todo next pause point

--- test/coord_helper_test.exs
+++ test/coord_helper_test.exs
@@ -11,26 +11,18 @@ defmodule AoCSharedTest.CoordHelper do
 
     dir_func = &Coord.right/1
 
-    stop_looking_when_func = &(not Coord.in_range?(&1, x_range, y_range))
-    found_collision_func = fn coord ->
+    give_up_when_func = &(not Coord.in_range?(&1, x_range, y_range))
+    found_collision_func = fn target_coord ->
       board = %{
-        {7,1} => :knight
-        {1,7} => :pawn
-        {99,99} => :batman
+        {7,1} => :knight,
+        {1,7} => :pawn,
+        {99,99} => :batman,
       }
 
-      if Map.has_key?(board, coord) do
-        Map.get(board, coord)
-      end
-      case Map.get(board, coord) do
-        ans -> ans
-        nil -> nil
-      end
-
-      #alt (try rewriting the above checker with Enum.find_value. Not really important tho
-      Enum.find_value(board, fn {coord, value} ->
-        Map.get(board, coord)
-      end)
+      Enum.find(fn {board_coord, _value} -> board_coord == target_coord end)
     end
+
+    # TODO NOTE This might just end up in Coord module
+    assert CoordHelper.first_in_direction(start, dir_func, give_up_when_func, found_collision_func) == {{7,1}, :knight}
   end
 end
