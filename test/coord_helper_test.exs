a9454facaa87325e64f5a3d79bc1f576cb1f8b77
Eric Ridgeway <ericridgeway@gmail.com>
2023-11-22 09:55:44 -0500

    Start shared funcs for slide coord until...

--- test/coord_helper_test.exs
+++ test/coord_helper_test.exs
@@ -2,4 +2,15 @@ defmodule AoCSharedTest.CoordHelper do
   use ExUnit.Case
 
   alias AoCShared.CoordHelper
+  alias AoCShared.Coord
+
+  # test "First coord in dir" do
+  #   start = {1,1}
+  #   dir_func = &Coord.right/1)
+
+  #   # TODO "fell off board?" in doc or rename?
+  #   stop_looking_when_func = fn coord ->
+
+  #   end
+  # end
 end
