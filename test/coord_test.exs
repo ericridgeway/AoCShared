0d93760f0c48a44e051c1e318cbe2e69c9391294
Eric Ridgeway <ericridgeway@gmail.com>
2023-11-22 10:09:27 -0500

    Tests for Coord.inRange?

--- test/coord_test.exs
+++ test/coord_test.exs
@@ -21,4 +21,24 @@ defmodule AoCSharedTest.Coord do
       {3,3},
     ])
   end
+
+  describe "in_range" do
+    @x_range 1..10
+    @y_range 1..2
+    # TODO Doc note: range is inclusive
+
+    test "Solidly in" do
+      assert Coord.in_range?({2,2}, x_range, y_range)
+    end
+
+    test "Solidly out" do
+      refute Coord.in_range?({0,0}, x_range, y_range)
+      refute Coord.in_range?({99,99}, x_range, y_range)
+    end
+
+    test "Off only one of the edges" do
+      refute Coord.in_range?({1,99}, x_range, y_range)
+      refute Coord.in_range?({-99,1}, x_range, y_range)
+    end
+  end
 end
