a6babd5a1cdcf759a09d2cf0bd2fb0ceadd45a8b
Eric Ridgeway <ericridgeway@gmail.com>
2023-11-22 10:48:09 -0500

    Sketch out ideas for checker

--- test/coord_helper_test.exs
+++ test/coord_helper_test.exs
@@ -4,13 +4,33 @@ defmodule AoCSharedTest.CoordHelper do
   alias AoCShared.CoordHelper
   alias AoCShared.Coord
 
-  # test "First coord in dir" do
-  #   start = {1,1}
-  #   dir_func = &Coord.right/1)
+  test "First coord in dir" do
+    start = {1,1}
+    x_range = 1..10
+    y_range = 1..10
 
-  #   # TODO "fell off board?" in doc or rename?
-  #   stop_looking_when_func = fn coord ->
+    dir_func = &Coord.right/1
 
-  #   end
-  # end
+    stop_looking_when_func = &(not Coord.in_range?(&1, x_range, y_range))
+    found_collision_func = fn coord ->
+      board = %{
+        {7,1} => :knight
+        {1,7} => :pawn
+        {99,99} => :batman
+      }
+
+      if Map.has_key?(board, coord) do
+        Map.get(board, coord)
+      end
+      case Map.get(board, coord) do
+        ans -> ans
+        nil -> nil
+      end
+
+      #alt (try rewriting the above checker with Enum.find_value. Not really important tho
+      Enum.find_value(board, fn {coord, value} ->
+        Map.get(board, coord)
+      end)
+    end
+  end
 end
