ca6b0805201175e8bc9e87b554939d18ff991ce1
Eric Ridgeway <ericridgeway@gmail.com>
2023-11-23 15:04:51 -0500

    Fix a typo/bug and tighten the cond a little

--- lib/ao_c_shared/coord_helper.ex
+++ lib/ao_c_shared/coord_helper.ex
@@ -3,17 +3,12 @@ defmodule AoCShared.CoordHelper do
   def first_in_direction(t, direction_func, give_up_when_func, find_collision_func) do
     loop = &first_in_direction(&1, direction_func, give_up_when_func, find_collision_func)
     slide = direction_func.(t)
+    result = find_collision_func.(slide)
 
     cond do
       give_up_when_func.(slide) -> :found_no_collision
-      true ->
-        result = find_collision_func.(t)
-
-        if result do
-          result
-        else
-          loop.(slide)
-        end
+      result -> result
+      true -> loop.(slide)
     end
   end
 end
