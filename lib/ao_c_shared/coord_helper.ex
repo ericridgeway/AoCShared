ed4c287e82a58f13b24a191f5627ba447f1fb23a
Eric Ridgeway <ericridgeway@gmail.com>
2023-11-23 15:08:55 -0500

    For revert, ugh, much worse

--- lib/ao_c_shared/coord_helper.ex
+++ lib/ao_c_shared/coord_helper.ex
@@ -3,12 +3,14 @@ defmodule AoCShared.CoordHelper do
   def first_in_direction(t, direction_func, give_up_when_func, find_collision_func) do
     loop = &first_in_direction(&1, direction_func, give_up_when_func, find_collision_func)
     slide = direction_func.(t)
-    result = find_collision_func.(slide)
 
     cond do
       give_up_when_func.(slide) -> :found_no_collision
-      result -> result
-      true -> loop.(slide)
+      true ->
+        case find_collision_func.(slide) do
+          nil -> loop.(slide)
+          result -> result
+        end
     end
   end
 end
