2b6366f6048419f2bc2eccacf0d95c829e193e4e
Eric Ridgeway <ericridgeway@gmail.com>
2023-11-23 15:27:14 -0500

    Add types, and keeping messing with the cond. I think this might actually need to be a With

--- lib/ao_c_shared/coord_helper.ex
+++ lib/ao_c_shared/coord_helper.ex
@@ -1,13 +1,23 @@
 defmodule AoCShared.CoordHelper do
+  alias AoCShared.Coord
 
+  @type t :: Coord.t
+
+  @type found_collision :: any
+
+  @type direction_func :: (t -> t)
+  @type give_up_when_func :: (t -> boolean)
+  @type find_collision_func :: (t -> found_collision | nil)
+
+  @spec first_in_direction(t, direction_func, give_up_when_func, find_collision_func) :: found_collision | :found_no_collision
   def first_in_direction(t, direction_func, give_up_when_func, find_collision_func) do
     loop = &first_in_direction(&1, direction_func, give_up_when_func, find_collision_func)
+
     slide = direction_func.(t)
-    result = find_collision_func.(slide)
 
     cond do
       give_up_when_func.(slide) -> :found_no_collision
-      result -> result
+      find_collision_func.(slide) = result -> result
       true -> loop.(slide)
     end
   end
