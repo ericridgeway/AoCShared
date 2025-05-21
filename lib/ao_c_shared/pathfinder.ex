c54018dfd17ff3b830c03b0dba15bb5155353350
Eric Ridgeway <ericridgeway@gmail.com>
2025-05-21 19:09:48 -0400

    Start making a master Pathfinder

---
+++ lib/ao_c_shared/pathfinder.ex
@@ -0,0 +1,9 @@
+defmodule AoCShared.Pathfinder do
+  @moduledoc """
+  Copied from previous version, my AoC 2022 Day 16
+  """
+
+  # TODO doc note, Each key stores it's parent who gets it closest back to original source
+  def parents() do
+  end
+end
