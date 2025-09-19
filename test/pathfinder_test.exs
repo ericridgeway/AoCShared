bf4edab2f688d7b669bf7d65b73a5f1b731cad0e
Eric Ridgeway <ericridgeway@gmail.com>
2025-09-18 20:53:29 -0400

    Comment out pathfinder test which I think I left partway done

--- test/pathfinder_test.exs
+++ test/pathfinder_test.exs
@@ -1,19 +1,20 @@
 defmodule AoCSharedTest.Pathfinder do
-  use ExUnit.Case
-  alias AoCShared.Pathfinder
-  alias AoCShared.Coord
+  # use ExUnit.Case
+  # alias AoCShared.Ascii
+  # alias AoCShared.Pathfinder
+  # alias AoCShared.Coord
 
-  test "All neighbors" do
-    board = """
-    ...
-    ...
-    ...
-    """
-    |> Ascii.to_map
-    source = {1,1}
-    neighbors_func = &Coord.all_adjacent/1
+  # test "All neighbors" do
+  #   board = """
+  #   ...
+  #   ...
+  #   ...
+  #   """
+  #   |> Ascii.to_map
+  #   source = {1,1}
+  #   neighbors_func = &Coord.all_adjacent/1
 
-    parents = Pathfinder.parents(board, source, neighbors_func)
-    assert Pathfinder.parent_path(parents, {3,3} == [{1,1}, {2,2}, {3,3}]
-  end
+  #   parents = Pathfinder.parents(board, source, neighbors_func)
+  #   assert Pathfinder.parent_path(parents, {3,3} == [{1,1}, {2,2}, {3,3}])
+  # end
 end
